97d563d179f148252e7db2b9b1da68b2df3cf384
Ariel Himmelstern <arhimmel@gmail.com>
2026-03-31 18:07:34 -0600

    make podman work

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,10 +1,14 @@
 use anyhow::Result;
-use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
+use sqlx::{sqlite::{SqliteConnectOptions, SqlitePoolOptions}, SqlitePool};
+use std::str::FromStr;
 
 pub async fn init_pool(database_url: &str) -> Result<SqlitePool> {
+    let options = SqliteConnectOptions::from_str(database_url)?
+        .create_if_missing(true);
+
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
-        .connect(database_url)
+        .connect_with(options)
         .await?;
 
     sqlx::migrate!("./migrations").run(&pool).await?;
